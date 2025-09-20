9b96ea0f94a163090ae118f3432303d8184641ac
Slava Zavhorodniy <40838788+zavgorodniyvv@users.noreply.github.com>
2025-09-20 12:42:23 +0300

    Merge pull request #1
    * Add `Person` model and SWAPI person fetch logic

--- main.go
+++ main.go
@@ -1,21 +1,44 @@
 package main
 
 import (
-  "fmt"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"swapi/models"
 )
 
-//TIP <p>To run your code, right-click the code and select <b>Run</b>.</p> <p>Alternatively, click
-// the <icon src="AllIcons.Actions.Execute"/> icon in the gutter and select the <b>Run</b> menu item from here.</p>
-
 func main() {
-  //TIP <p>Press <shortcut actionId="ShowIntentionActions"/> when your caret is at the underlined text
-  // to see how GoLand suggests fixing the warning.</p><p>Alternatively, if available, click the lightbulb to view possible fixes.</p>
-  s := "gopher"
-  fmt.Printf("Hello and welcome, %s!\n", s)
+	person, err := models.NewPerson("")
+	if err != nil {
+		fmt.Println(err)
+	}
 
-  for i := 1; i <= 5; i++ {
-	//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-	// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-	fmt.Println("i =", 100/i)
-  }
+	fmt.Println(person)
+
+	url := "https://swapi.dev/api/people/1/"
+
+	client := &http.Client{}
+
+	response, err := client.Get(url)
+	if err != nil {
+		fmt.Printf("Error making request: %v\n", err)
+		return
+	}
+	defer response.Body.Close()
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("Error reading response: %v\n", err)
+		return
+	}
+
+	err = json.Unmarshal(body, &person)
+	if err != nil {
+		fmt.Printf("Error parsing JSON: %v\n", err)
+		return
+	}
+
+	fmt.Println(person)
+
 }
